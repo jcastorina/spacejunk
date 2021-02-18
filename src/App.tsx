fbbdcaa8e238edea7f07ec5a4e3aa57eb4a5e58a
Joseph Castorina <jcastorina@gmail.com>
2021-02-18 13:30:51 -0700

    update app to add new comment

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Provider } from "urql";
 import { client } from "./lib/api";
 import { Main } from "./features/Main";
 
+// i am adding new content
+
 function App() {
   return (
     <Provider value={client}>
